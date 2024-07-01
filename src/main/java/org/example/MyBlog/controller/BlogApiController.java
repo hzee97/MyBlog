35b4d334ef654a8c9628ceb0304565736ea13568
hzee97 <janghyeonji0830@naver.com>
2024-07-02 03:18:53 +0900

    :sparkles: 블로그 내 모든 글 목록 조회 API 구현.

--- src/main/java/org/example/MyBlog/controller/BlogApiController.java
+++ src/main/java/org/example/MyBlog/controller/BlogApiController.java
@@ -3,12 +3,16 @@ package org.example.MyBlog.controller;
 import lombok.RequiredArgsConstructor;
 import org.example.MyBlog.domain.Article;
 import org.example.MyBlog.dto.AddArticleRequest;
+import org.example.MyBlog.dto.ArticleResponse;
 import org.example.MyBlog.service.BlogService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController  // HTTP Response Body 에 객체 데이터를 JSON 형식으로 반환하는 컨트롤러.
@@ -20,5 +24,16 @@ public class BlogApiController {
         Article savedArticle = blogService.save(request);
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(savedArticle);
+    }
+
+    @GetMapping("/api/articles")
+    public ResponseEntity<List<ArticleResponse>> findAllArticles(){
+        List<ArticleResponse> articles = blogService.findAll()
+                .stream()
+                .map(ArticleResponse::new)
+                .toList();
+
+        return ResponseEntity.ok()
+                .body(articles);
     }
 }
--- src/main/java/org/example/MyBlog/service/BlogService.java
+++ src/main/java/org/example/MyBlog/service/BlogService.java
@@ -6,6 +6,8 @@ import org.example.MyBlog.dto.AddArticleRequest;
 import org.example.MyBlog.repository.BlogRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @RequiredArgsConstructor  // final or @NotNull 이 붙은 필드의 생성자 추가.
 @Service
 public class BlogService {
@@ -14,5 +16,10 @@ public class BlogService {
     // 블로그 글 추가 메서드
     public Article save(AddArticleRequest request){
         return blogRepository.save(request.toEntity());
+    }
+
+    // 전체 글 조회 메서드
+    public List<Article> findAll() {
+        return blogRepository.findAll();
     }
 }
