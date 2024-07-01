eb6e4161d2d8e13dbfc873872920b1ec7c7d6bf7
hzee97 <janghyeonji0830@naver.com>
2024-07-02 04:16:59 +0900

    :sparkles: 해당 글 내용 조회하는 API 구현.

--- src/main/java/org/example/MyBlog/controller/BlogApiController.java
+++ src/main/java/org/example/MyBlog/controller/BlogApiController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,6 +25,14 @@ public class BlogApiController {
         Article savedArticle = blogService.save(request);
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(savedArticle);
+    }
+
+    @GetMapping("api/articles/{id}")
+    public ResponseEntity<ArticleResponse> findArticle(@PathVariable long id){
+        Article article = blogService.findById(id);
+
+        return ResponseEntity.ok()
+                .body(new ArticleResponse(article));
     }
 
     @GetMapping("/api/articles")
