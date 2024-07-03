75e863941477e6be25065e82993a4f067244a7ff
hzee97 <janghyeonji0830@naver.com>
2024-07-04 02:50:45 +0900

    :sparkles: 블로그 글 조회 구현.

---
+++ src/main/java/org/example/MyBlog/dto/ArticleViewResponse.java
@@ -0,0 +1,27 @@
+package org.example.MyBlog.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.example.MyBlog.domain.Article;
+import org.springframework.cglib.core.Local;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@Getter
+public class ArticleViewResponse {
+    private Long id;
+    private String title;
+    private String content;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public ArticleViewResponse(Article article){
+        this.id = article.getId();
+        this.title = article.getTitle();
+        this.content = article.getContent();
+        this.createdAt = article.getCreatedAt();
+        this.updatedAt = article.getUpdatedAt();
+    }
+
+}
