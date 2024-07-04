1bf742f161541fc9655d46aeb8bf83551c238343
hzee97 <janghyeonji0830@naver.com>
2024-07-04 21:46:04 +0900

    :sparkles: 로그아웃 구현.

--- src/main/java/org/example/MyBlog/controller/UserApiController.java
+++ src/main/java/org/example/MyBlog/controller/UserApiController.java
@@ -1,9 +1,15 @@
 package org.example.MyBlog.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.example.MyBlog.dto.AddUserRequest;
 import org.example.MyBlog.service.UserService;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @RequiredArgsConstructor
@@ -14,6 +20,12 @@ public class UserApiController {
     @PostMapping("/user")
     public String signup(AddUserRequest request) {
         userService.save(request);
+        return "redirect:/login";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response){
+        new SecurityContextLogoutHandler().logout(request,response, SecurityContextHolder.getContext().getAuthentication());
         return "redirect:/login";
     }
 }
