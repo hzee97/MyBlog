17b952707d70f440b0b0a8b21da54193959c1aac
hzee97 <janghyeonji0830@naver.com>
2024-07-05 17:51:39 +0900

    :sparkles: jwt 토큰 생성.

---
+++ src/main/java/org/example/MyBlog/config/jwt/TokenProvider.java
@@ -0,0 +1,39 @@
+package org.example.MyBlog.config.jwt;
+
+import io.jsonwebtoken.Header;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.RequiredArgsConstructor;
+import org.example.MyBlog.domain.User;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.util.Date;
+
+
+@RequiredArgsConstructor
+@Service
+public class TokenProvider {
+    private final JwtProperties jwtProperties;
+
+    public String generateToken(User user, Duration expiredAt){
+        Date now = new Date();
+        return makeToken(new Date(now.getTime()+expiredAt.toMillis()),user);
+    }
+
+    // JWT 토큰 생성
+    private String makeToken(Date expiry, User user){
+        Date now = new Date();
+
+        return Jwts.builder()
+                .setHeaderParam(Header.TYPE, Header.JWT_TYPE)
+                .setIssuer(jwtProperties.getIssuer())
+                .setIssuer(now)
+                .setExpiration(expiry)
+                .setSubject(user.getEmail())
+                .claim("id",user.getId())
+                .signWith(SignatureAlgorithm.HS256, jwtProperties.getSecretKey())
+                .compact();
+    }
+
+}
