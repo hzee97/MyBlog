62e4ece1c7d90f4cd68f362564e337462f310012
hzee97 <janghyeonji0830@naver.com>
2024-07-05 18:49:51 +0900

    :sparkles: refresh token 구현.

---
+++ src/main/java/org/example/MyBlog/repository/RefreshTokenRepository.java
@@ -0,0 +1,11 @@
+package org.example.MyBlog.repository;
+
+import org.example.MyBlog.domain.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByUserId(Long userId);
+    Optional<RefreshToken> findByRefreshToken(String refreshToken);
+}
