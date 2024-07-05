3264ad60b38a693b351eb2f8a4d7d4679094cd40
hzee97 <janghyeonji0830@naver.com>
2024-07-05 17:58:57 +0900

    :sparkles: jwt 토큰 구현.

--- src/main/java/org/example/MyBlog/config/jwt/TokenProvider.java
+++ src/main/java/org/example/MyBlog/config/jwt/TokenProvider.java
@@ -1,14 +1,20 @@
 package org.example.MyBlog.config.jwt;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Header;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.RequiredArgsConstructor;
 import org.example.MyBlog.domain.User;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Service;
 
 import java.time.Duration;
+import java.util.Collections;
 import java.util.Date;
+import java.util.Set;
 
 
 @RequiredArgsConstructor
@@ -36,4 +42,36 @@ public class TokenProvider {
                 .compact();
     }
 
+    // JWT 토큰 유효성 검증
+    public boolean validToken(String token){
+        try{
+            Jwts.parsser()
+                    .setSigninfKey(jwtProperties.getSecretKey())
+                    .parseClaimsJws(token);
+            return true;
+        } catch (Exception e){
+          return false;
+        }
+    }
+
+    // 토큰 기반 인증 정보 가져오기.
+    public Authentication getAuthentication(String token){
+        Claims claims = getClaims(token);
+        Set<SimpleGrantedAuthority> authorities = Collections.singleton(new SimpleGrantedAuthority("ROLE_USER"));
+
+        return new UsernamePasswordAuthenticationToken(new org.springframework.security.core.userdetails.User(claims.getSubject(),"",authorities),token,authorities);
+    }
+
+    // 토큰 기반으로 유저 id 가져오기
+    public Long getUserId(String token){
+        Claims claims = getClaims(token);
+        return claims.get("id",Long.class);
+    }
+
+    private Claims getClaims(String token){
+        return Jwts.parser()
+                .setSigningKey(jwtProperties.getSecretKey())
+                .parseClaimsJws(token)
+                .getBody();
+    }
 }
