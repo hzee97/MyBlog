81d04823b549c4b9c42369a491c41d06428f4ed7
hzee97 <janghyeonji0830@naver.com>
2024-07-07 01:08:15 +0900

    :white_check_mark: test code

---
+++ src/test/java/org/example/MyBlog/controller/TokenApiControllerTest.java
@@ -0,0 +1,93 @@
+package org.example.MyBlog.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.example.MyBlog.config.jwt.JwtProperties;
+import org.example.MyBlog.controller.config.jwt.JwtFactory;
+import org.example.MyBlog.domain.RefreshToken;
+import org.example.MyBlog.domain.User;
+import org.example.MyBlog.dto.CreateAccessTokenRequest;
+import org.example.MyBlog.repository.RefreshTokenRepository;
+import org.example.MyBlog.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import static org.junit.jupiter.api.Assertions.*;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.Map;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class TokenApiControllerTest {
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @Autowired
+    protected ObjectMapper objectMapper;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @Autowired
+    JwtProperties jwtProperties;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    RefreshTokenRepository refreshTokenRepository;
+
+    @BeforeEach
+    public void mockMvcSetup(){
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(context)
+                .build();
+        userRepository.deleteAll();;
+    }
+
+    @DisplayName("createNewAccessToken : 새로운 액세스 토큰 발급.")
+    @Test
+    public void createNewAccessToken() throws Exception{
+        // given
+        final String url = "/api/token";
+
+        User testUser = userRepository.save(User.builder()
+                .email("user@gmail.com")
+                .password("test123")
+                .build());
+
+        String refreshToken = JwtFactory.builder()
+                .claims(Map.of("id", testUser.getId()))
+                .build()
+                .createToken(jwtProperties);
+
+        refreshTokenRepository.save(new RefreshToken(testUser.getId(), refreshToken));
+
+        CreateAccessTokenRequest request = new CreateAccessTokenRequest();
+        request.setRefreshToken(refreshToken);
+
+        final String requestBody = objectMapper.writeValueAsString(request);
+
+        // when
+        ResultActions resultActions = mockMvc.perform(post(url)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(requestBody));
+
+        // then
+        resultActions
+                .andExpect(status().isCreated())
+                .andExpect((ResultMatcher) jsonPath("$.accessToken").isNotEmpty());
+    }
+}
